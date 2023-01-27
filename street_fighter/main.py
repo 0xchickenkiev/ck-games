74e2e48e0f566d44e64f141d92642934125eb5de
Kevin Edwards <kevin_edwards91@hotmail.com>
2023-01-27 17:05:31 +0000

    Adding assets and background

--- street_fighter/main.py
+++ street_fighter/main.py
@@ -6,17 +6,30 @@ pygame.init()
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
-scren = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
 pygame.display.set_caption("Street Fighter")
+
+#load background image
+bg_image = pygame.image.load("street_fighter/assets/images/background/background.jpg").convert_alpha()
+
+#function for drawing background
+def draw_bg():
+    screen.blit(bg_image, (0,0))
 
 #game loop
 run = True
 while run:
 
+    #draw background
+    draw_bg()
+
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    #update display
+    pygame.display.update()
 
 #exit pygame
 pygame.quit()
