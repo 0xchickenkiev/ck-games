ca1780b7c6120bee40a37ef13c54de1c549c848a
Kevin Edwards <kevin_edwards91@hotmail.com>
2023-03-19 07:58:55 +0000

    band_name_generator

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+import time
+
+print("Welcome to the Band Name Generator")
+time.sleep(1)
+
+city = input("What's the name of the city you grew up in? ")
+time.sleep(1)
+
+pet = input("What's your pets name? ")
