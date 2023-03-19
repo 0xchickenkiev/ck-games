d26bdddd25f32fb5b6f26160a1f3b4d3e58ecb79
Kevin Edwards <kevin_edwards91@hotmail.com>
2023-03-19 15:21:21 +0000

    Add inputs

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,12 @@
 print("Welcome to the PyPassword Generator")
+
+#Ask how long they would like the password to be
+pw_length = int(input("How long would you like your password to be?\n"))
+
+#Ask how many numbers thet would like in the password
+pw_numbers = int(input("How many numbers would you like in your password?\n"))
+
+#Ask how many symbols they would like in the password
+pw_symbol = int(input("How many symbols would you like in your password?\n"))
+
+#print generated password
