4d13f8bd918c1573898a9a273200a38276650743
Kevin Edwards <kevin_edwards91@hotmail.com>
2023-03-19 08:05:04 +0000

    Adding newline

--- band_name_generator.py
+++ band_name_generator.py
@@ -3,10 +3,10 @@ import time
 print("Welcome to the Band Name Generator")
 time.sleep(1)
 
-city = input("What's the name of the city you grew up in? ")
+city = input("What's the name of the city you grew up in?\n")
 time.sleep(1)
 
-pet = input("What's your pets name? ")
+pet = input("What's your pets name?\n")
 time.sleep(1)
 
 print("Your band name could be " + city + " " + pet)
